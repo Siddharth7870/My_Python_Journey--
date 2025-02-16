3708adab799ccece33eb7cb06b77da1b39d7fc17
Sidharth7870 <ramanandkumar0770@gmail.com>
2025-02-16 14:53:03 +0530

    Output

--- Chapter-01/Practice_Question/3.Practice.py
+++ Chapter-01/Practice_Question/3.Practice.py
@@ -1,1 +1,6 @@
 # Install an external module and use it to perform an operation of your interest
+
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("Your name is Ramanand and your title Siddhart i am write now yes no")
+engine.runAndWait()
