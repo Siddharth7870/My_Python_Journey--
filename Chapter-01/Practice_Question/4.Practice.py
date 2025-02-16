8bd6d1dfec2100af69bf99beb62e7d7dfa7bc8cb
Sidharth7870 <ramanandkumar0770@gmail.com>
2025-02-16 18:49:38 +0530

    code

--- Chapter-01/Practice_Question/4.Practice.py
+++ Chapter-01/Practice_Question/4.Practice.py
@@ -1,2 +1,15 @@
 # Write a python program to print the contents of a directory using the os module.
 # Search online for the function which does that
+
+import os
+
+# Specify the directory (use '.' for the current directory)
+directory_path = "/Apps"
+
+# Get the list of files and directories
+contents = os.listdir(directory_path)
+
+# Print the contents
+print("Contents of the directory:", directory_path)
+for item in contents:
+    print(item)
