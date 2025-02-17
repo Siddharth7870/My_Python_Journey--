9141be8a537a83b827aa1e1c61e8525993b2ff73
Sidharth7870 <ramanandkumar0770@gmail.com>
2025-02-17 20:27:08 +0530

    Variable

---
+++ Chapter-02/01_variables.py
@@ -0,0 +1,10 @@
+# A  is a variable or ( contenar ) for store  5 values or number
+A = 5 #  for store integer value
+
+# B  is a variable or ( contenar ) for store  7.2 values or number
+B = 7.2 # for  stote floating point numbers
+
+C = "Sum is =  " # for storer String or char value
+
+print( C ) # O/P = Sum is =
+print( A + B  ) # O/P =  12.2
