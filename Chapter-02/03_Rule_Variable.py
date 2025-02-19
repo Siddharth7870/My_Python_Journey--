1f92e3ddd1ae1a0c467106650d0fc2ea182c72db
Sidharth7870 <ramanandkumar0770@gmail.com>
2025-02-19 11:04:22 +0530

    Rule of variable

---
+++ Chapter-02/03_Rule_Variable.py
@@ -0,0 +1,16 @@
+A = 99 # This is a valid variable( integer types of keyword )
+
+B = 9.9 # This is a valid variable ( float types of keyword )
+
+Name = "Ramanand" # This is a valid variable ( string types of keyword) 
+
+
+Ram_Shyam = " As a friend " # This is a valid variable because in use underscore
+
+# Rama$nand = 99  # This is a invalid variable because in use specific character
+ 
+
+
+
+
+
