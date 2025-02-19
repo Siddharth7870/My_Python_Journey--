42811902bc2a67aeac175e1ce1f3554014752528
Sidharth7870 <ramanandkumar0770@gmail.com>
2025-02-19 10:44:21 +0530

    Datatpes

---
+++ Chapter-02/02_datatypes.py
@@ -0,0 +1,9 @@
+a = 2 # a is an integer
+
+b = 5.33 # b is a floating point number
+
+c = "Bhimrao Ambedkar" # c is a string
+
+d = False # d is a bollean variable
+
+e = None # e is a none type variable
