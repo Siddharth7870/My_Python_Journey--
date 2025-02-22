38e4943ffeca322ba89656629789e32f238555e5
Sidharth7870 <ramanandkumar0770@gmail.com>
2025-02-22 14:49:35 +0530

    Logica Operator

--- Chapter-02/Variable_and_Datatype/04_Operators.py
+++ Chapter-02/Variable_and_Datatype/04_Operators.py
@@ -13,20 +13,20 @@ print( a )
 print(" For 6 assign value in b variable")
 b = 6 
 
-print(" for sum( -= ) of assignment operator")
-b += 3  # Increment the value of b by 3 and then assing it to b
+print(" for sum( -= ) or assignment operator")
+b += 3  # Increment the value or b by 3 and then assing it to b
 print( b )  # Output Will be 9
 
-print("For substraction ( -= ) of assignment operator ")
-b -= 3  # Decrement the value of b by 3 and then assign it to b
+print("For substraction ( -= ) or assignment operator ")
+b -= 3  # Decrement the value or b by 3 and then assign it to b
 print( b )  # Output will be  6
 
-print(" For multiplication ( *= ) of assignment operator ")
-b *= 3  # Multiple the value of b by 3 and then assing it to b
+print(" For multiplication ( *= ) or assignment operator ")
+b *= 3  # Multiple the value or b by 3 and then assing it to b
 print( b )  # Output will be 18
 
-print(" For divide ( /= ) of assignment operator ")
-b /= 3  #  Division the value of b by 3 and then assing it to b 
+print(" For divide ( /= ) or assignment operator ")
+b /= 3  #  Division the value or b by 3 and then assing it to b 
 print( b )  # Output will be 6
 
 # Comparison Operators
@@ -38,3 +38,21 @@ print(e)
 
 f = 4 == 4
 print(f)
+
+
+# Logical Operators
+
+# or
+print( " logical for or ")
+e = True or False
+print(" True or False is ", True or False) # True
+print( " True or True is ", True or True ) # True
+print( " Fasle or True is ", False or True ) # True
+print( " False or False is ", False or False ) # Fasle
+
+print( " Logical for and ")
+# and
+print(" True and False is ", True or False) # Fasle
+print( " True and True is ", True or True ) # True
+print( " False and False is ", False or False ) # False
+print( " False and Treu is ", False or True )  # False
