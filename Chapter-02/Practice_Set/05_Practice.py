081facbf70a3d8c9f7b3933fff1c1037bbda4529
Sidharth7870 <ramanandkumar0770@gmail.com>
2025-02-22 16:06:28 +0530

    Find of Average no

---
+++ Chapter-02/Practice_Set/05_Practice.py
@@ -0,0 +1,5 @@
+print(''' Write a python program to calculate the ssuare of a number entered by the user. ''')
+
+x = int(input("Enter any number = "))
+squere = x * x
+print("Square is = ",squere)
---
+++ Chapter-02/Practice_Set/06_Practice.py
@@ -0,0 +1,6 @@
+print(''' Write a python program to find an average of two numbers entered by the user ''')
+
+no1 = int(input("Enter a number 1 = "))
+no2 = int(input("Enter a number 2 = "))
+sum = no1 + no2
+print("Average are two numbers = ", sum / 2)
