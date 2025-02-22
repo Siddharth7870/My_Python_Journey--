bb6aa377c1e939a453672d0775396713513bf9bd
Sidharth7870 <ramanandkumar0770@gmail.com>
2025-02-22 15:26:45 +0530

    Remainder code

---
+++ Chapter-02/Practice_Set/02_Practice.py
@@ -0,0 +1,8 @@
+print( " Write a python program to find remainder when a number is divided by 2")
+
+a = 19 
+b = 5
+print("Remainder when a is divided by b is ", a % b )
+
+a = int(input( "Enter any number "))
+print( "For remainder " , a % 5 )
