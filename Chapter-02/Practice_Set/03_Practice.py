ea64cab1208eff0a2bca58a21ce44098b7ac75dd
Sidharth7870 <ramanandkumar0770@gmail.com>
2025-02-22 15:32:19 +0530

    Check Variable type

---
+++ Chapter-02/Practice_Set/03_Practice.py
@@ -0,0 +1,13 @@
+print(" Check the type of variable assigned using input () function")
+
+
+a = 34
+name = "Siddharth"
+f = 2.3
+t = True and False
+n = None
+print( type(a))
+print( type(name))
+print( type(f))
+print( type(t))
+print( type( n ))
