9ed074e6c074765a7d7a49a22c285c1728cba9c4
Sidharth7870 <ramanandkumar0770@gmail.com>
2025-02-22 15:55:20 +0530

    Comparision

---
+++ Chapter-02/Practice_Set/04_Practice.py
@@ -0,0 +1,8 @@
+print('''Q.04->  Use copmparison operator to find out whether
+a given variable a is greater than 'b'  or not. Take a = 34 and b = 80 ''')
+
+print("here is use comparison Operator ")
+a = 34
+b = 80
+com = a>b
+print("Compariso is : ",com)
