82ead3f3fd15d41579f06ef09ee2430aa0555218
Sidharth7870 <ramanandkumar0770@gmail.com>
2025-02-22 13:39:23 +0530

    New folder

---
+++ Chapter-02/Variable_and_Datatype/04_Operators.py
@@ -0,0 +1,30 @@
+# Arithmetic Operators
+A = 19
+B = 3
+print(" for Arithmetic Operator ( + , - , * , / , % ) ")
+C = A + B 
+print( C )
+
+
+#  Assignment Operators
+print("Assignment Operators")
+a = 4 - 2   # Assign 4 - 2 in a
+print( a )
+print(" For 6 assign value in b variable")
+b = 6 
+
+print(" for sum( -= ) of assignment operator")
+b += 3  # Increment the value of b by 3 and then assing it to b
+print( b )  # Output Will be 9
+
+print("For substraction ( -= ) of assignment operator ")
+b -= 3  # Decrement the value of b by 3 and then assign it to b
+print( b )  # Output will be  6
+
+print(" For multiplication ( *= ) of assignment operator ")
+b *= 3  # Multiple the value of b by 3 and then assing it to b
+print( b )  # Output will be 18
+
+print(" For divide ( /= ) of assignment operator ")
+b /= 3  #  Division the value of b by 3 and then assing it to b 
+print( b )  # Output will be 6
